411bf9604b44a5fa530ecd25da2cffebd2aa87a0
Jonesey86 <irj082024@gmail.com>
2025-02-03 14:15:29 +0100

    edits to bmi categories

--- obj/Debug/net8.0/BMI-Calculator.AssemblyInfo.cs
+++ obj/Debug/net8.0/BMI-Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BMI-Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6c05f3a3d952f13cbdba18ca292efde392ab36a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e64acd80ea332bf3f20c03940e891afb02f60d62")]
 [assembly: System.Reflection.AssemblyProductAttribute("BMI-Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BMI-Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
