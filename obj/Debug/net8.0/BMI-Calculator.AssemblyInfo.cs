0d26f0f36292c1648148ac5c6d550c5ec53c1335
Jonesey86 <irj082024@gmail.com>
2025-03-05 09:34:06 +0100

    initial commit

--- obj/Debug/net8.0/BMI-Calculator.AssemblyInfo.cs
+++ obj/Debug/net8.0/BMI-Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BMI-Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2e29508c18d8995f6aa3d04b9ff4a238941d457")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b33c9649895801442fcb6f615eeda22bf5db3dd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("BMI-Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BMI-Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
