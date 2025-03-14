72b9f4264480447937f90e46739319cd4e113bfb
Jonesey86 <irj082024@gmail.com>
2025-03-14 09:16:01 +0100

    terminal test

--- obj/Debug/net8.0/BMI-Calculator.AssemblyInfo.cs
+++ obj/Debug/net8.0/BMI-Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BMI-Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ceb0652b682617b3ba0a0eb823fb25f5e784e379")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e85ea3a39191d5679df0434320485dab643dcd5")]
 [assembly: System.Reflection.AssemblyProductAttribute("BMI-Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BMI-Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
